707fc092c25aac1a3a8d53b0b7c6534b25e1214d
Skiller1337 <artemgaame@gmail.com>
2023-09-14 16:58:15 +0300

    Initial commit

---
+++ UniProject/Controllers/CompanyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniProject.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+                var company = new CompanyModel
+                {
+                    Name = "Streamdigo Inc.",
+                    YearFounded = 2000,
+                    Location = "NewYork"
+                };
+
+                Random random = new Random();
+                int randomNumber = random.Next(0, 101);
+
+                company.RandomNumber = randomNumber;
+
+                return View(company);
+            }
+        }
+    }
---
+++ UniProject/Models/CompanyModel.cs
@@ -0,0 +1,7 @@
+public class CompanyModel
+{
+    public string Name { get; set; }
+    public int? YearFounded { get; set; }
+    public string? Location { get; set; }
+    public int? RandomNumber { get; set; }
+}
